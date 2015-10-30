7330e48aaccfbde7f25f5820025afb0f39d3d8cf
Reyzan <fauzan.azhari95@gmail.com>
2015-10-30 16:07:06 +0700

    added tugas komeng
    hahahah

---
+++ Maze/Assets/BehaviorLion.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BehaviorLion : MonoBehaviour {
+
+	public float speed = 0.05f;
+	public float speedSprint = 0.1f;
+	public float stamina = 100f;
+	public float totalStamina = 100f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//update yg fix
+	void FixedUpdate(){
+		float directionX = Input.GetAxisRaw("Horizontal");
+		float directionY = Input.GetAxisRaw("Vertical");
+		Vector3 posisi = transform.position;
+		if (Input.GetButton("Fire1") && stamina > 0f) {
+			posisi.y += speedSprint * directionY;
+			posisi.x += speedSprint * directionX;
+			stamina -= 4f;
+		} else {
+			posisi.y += speed * directionY;
+			posisi.x += speed * directionX;
+			if(stamina <= totalStamina && !Input.GetButton("Fire1")){
+				stamina += 2f;
+			}
+		}
+		transform.position = posisi;
+	}
+}
---
+++ Maze/Assets/CheckPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckPlayer : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	// nambahin animasi fadein and fadeout
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.CompareTag("Player")) {
+			// cara naif heheheheh
+			this.GetComponent<SpriteRenderer>().enabled = false;
+			//this.gameObject.SetActive(false);
+		}
+	}
+	void OnTriggerExit2D(Collider2D col){
+		if (col.CompareTag("Player")) {
+			//this.gameObject.SetActive(true);
+			this.GetComponent<SpriteRenderer>().enabled = true;
+		}
+	} 
+}
